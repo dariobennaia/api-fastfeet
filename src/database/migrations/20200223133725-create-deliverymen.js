ff14b42f58eb40b60fec80bb66813bf33e0d23b1
Dario Santos <dariobennaia@gmail.com>
2020-02-23 10:56:39 -0300

    Criaçao de migration entregador

---
+++ src/database/migrations/20200223133725-create-deliverymen.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('deliverymen', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      avatar_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('deliverymen');
+  },
+};
